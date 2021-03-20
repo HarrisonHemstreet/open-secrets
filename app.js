398c81baf69e7d4ae78bbd9adcf922fc9bb933c2
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-03-20 15:13:56 -0600

    added more psudeo code

---
+++ app.js
@@ -0,0 +1,18 @@
+//Make an API call to the Open-Secrets API for info on:
+//1.  candContrib: results of top contributors to specified candidate for a House or Senate seat or member of Congress. 
+//                 These are 6-year numbers for senators/Senate candidates; 2-year numbers for representatives/House candidates.
+
+
+
+
+
+//2. candIndByInd: Provides total contributed to specified candidate from specified industry. Senate data reflects 2-year totals.
+
+//3. candIndustry: candIndustry	Provides the top ten industries contributing to a specified candidate for a House or Senate seat           
+//                 or member of Congress. These are 6-year numbers for Senators/Senate candidates; 2-year total for 
+//                 Representatives/House candidates.
+
+//4. candSector: Returns data on a candidate's Sector contributions to specified candidate for a House or Senate seat or member 
+//               of Congress. These are 6 year numbers for Senators/Senate candidates; 2 years for Representatives/House candidates
+
+//5. candSummary: 	Method to access summary data for a candidate

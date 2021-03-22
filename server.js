a378dea7c14c8e8f2b5de57aa38e4c22a20e0a8a
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-03-21 23:21:58 -0600

    added error handling on registration. Working

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { pool } = require("./dbConfig");
 const PORT = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -13,16 +14,44 @@ app.get("/", (req, res) => {
 
 app.get("/users/register", (req, res) => {
     res.render("register");
-})
+});
     
 app.get("/users/login", (req, res) => {
     res.render("login");
-})
+});
 
 app.get("/users/dashboard", (req, res) => {
     res.render("dashboard", { user: "hhemstreet" });
+});
+
+app.post("/register", (req, res) => {
+    let { name, email, password, password2 } = req.body;
+
+    console.log({
+        name,
+        email,
+        password,
+        password2
+    })
+    let errors = [];
+
+    if(!name || !email || !password || !password2) {
+        errors.push({ message: "Please enter all fields" });
+    }
+
+    if(password.length < 6 ) {
+        errors.push({ message: "Password should be at least 6 characters" })
+    }
+
+    if(password != password2) {
+        errors.push({ message: "Passwords do not match" });
+    }
+
+    if(errors.length > 0) {
+        res.render('register', { errors })
+    }
 })
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-} )
+});

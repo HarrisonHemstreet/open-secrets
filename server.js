fb1c83e03691f8adb211c403a49db8a55d86fb90
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-03-22 01:23:10 -0600

    finished authentication for the most part

--- server.js
+++ server.js
@@ -2,28 +2,54 @@ const express = require("express");
 const app = express();
 const { pool } = require("./dbConfig");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("express-flash");
+const passport = require("passport");
+
+const initializePassport = require("./passportConfig");
+
+initializePassport(passport);
 
 const PORT = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
+app.use(session({
+    secret: "secret",
+
+    resave: false,
+
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
 app.get("/", (req, res) => {
     res.render("index");
 });
 
 
-app.get("/users/register", (req, res) => {
+app.get("/users/register", checkAuthenticated, (req, res) => {
     res.render("register");
 });
     
-app.get("/users/login", (req, res) => {
+app.get("/users/login", checkAuthenticated, (req, res) => {
     res.render("login");
 });
 
-app.get("/users/dashboard", (req, res) => {
-    res.render("dashboard", { user: "hhemstreet" });
+app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
+    res.render("dashboard", { user: req.user.name });
 });
+
+app.get("/users/logout", (req, res) => {
+    req.logout();
+    req.flash("success_msg", "You have logged out");
+    res.redirect("/users/login");
+})
 
 app.post("/register", async (req, res) => {
     let { name, email, password, password2 } = req.body;
@@ -68,12 +94,48 @@ app.post("/register", async (req, res) => {
                 if(results.rows.length > 0) {
                     errors.push({ message: "Email already registered"});
                     res.render("register", { errors })
+                } else {
+                    pool.query(
+                        "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, password", 
+                        [name, email, hashedPassword], 
+                        (err, results) => {
+                            if(err) {
+                                throw err;
+                            }
+                            
+                            console.log(results.rows);
+                            req.flash("success_msg", "You are now registered. Please log in");
+                            res.redirect("/users/login");
+                        }
+                    )
                 }
             }
         )
 
     }
-})
+});
+
+app.post("/login", passport.authenticate("local", {
+        successRedirect: "/users/dashboard",
+        failRedirect: "/users/login",
+        failureFlash: true
+    })
+);
+
+function checkAuthenticated(req, res, next) {
+    if(req.isAuthenticated()){
+        return res.redirect("/users/dashboard");
+    }
+    next();
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if(req.isAuthenticated()){
+       return next();
+    }
+
+    res.redirect("/users/login");
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
